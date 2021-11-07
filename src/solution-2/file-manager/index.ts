9a6fe46bb7a542e2ed237f51695b924fcc400380
Ali Mert Çakar <mail@alimert.net>
2021-11-07 17:03:01 +0300

    fix: bug

--- src/solution-2/file-manager/index.ts
+++ src/solution-2/file-manager/index.ts
@@ -40,7 +40,11 @@ export default class FileMoverImpl implements FileManager {
     try {
       const [folderId, fileId] = this.getFileById(sourceFileId);
       //TODO dont splice do it immutable
-      const fileToMove = nextFolderList[+folderId].files.splice(+fileId, +fileId)[0];
+      console.log(nextFolderList[0], 'nextFolderList 0');
+      const fileToMove = nextFolderList[+folderId].files.splice(+fileId, 1)[0];
+      console.log(nextFolderList[0], 'nextFolderList 0 1');
+      console.log(fileToMove, 'fileToMove');
+
       const [destinationFolder] = this.getFolderById(destinationFolderId);
       nextFolderList[+destinationFolder].files.push(fileToMove);
     } catch (e) {
